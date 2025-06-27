7cd1081bcfe454806d115e2e0c1774695cfdb3ef
pc <pavanmamidi096@gmail.com>
2025-06-27 09:05:01 +0530

    Second Commit

--- BST.java
+++ BST.java
@@ -3,7 +3,7 @@ public class BST {
     Node root;
     public void insert(int value){
 
-        root=insert(root,value); // Comment
+        root=insert(root,value); // Comment 2
     }
     public Node insert (Node node,int value){
         if(node==null){
